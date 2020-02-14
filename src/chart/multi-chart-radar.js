659e610704c3880f6cc82fb9f535a311694de4fb
christophe-g <christophe.geiser@gmail.com>
2020-02-14 15:55:48 +0100

    docs typo

--- src/chart/multi-chart-radar.js
+++ src/chart/multi-chart-radar.js
@@ -9,7 +9,7 @@ import { LineRadial }  from '../d3-wrapper/d3-shape.js';
  * ## Radar Chart
  * A radar chart is a graphical method of displaying multivariate data in the form of a two-dimensional chart of three or more quantitative variables represented on axes starting from the same point. The relative position and angle of the axes is typically uninformative, but various heuristics, such as algorithms that plot data as the maximal total area, can be applied to sort the variables (axes) into relative positions that reveal distinct correlations, trade-offs, and a multitude of other comparative measures.
  *
- * @element multi-radar-chart
+ * @element multi-chart-radar
  */
 class MultiChartRadar extends MultiContainerRadar {
 
