aac70330cad788ab895cc6889a188c23a7a7da91
christophe-g <christophe.geiser@gmail.com>
2020-02-19 15:47:27 +0100

    makes sure intro is selected

--- demo/multi-chart-demo.js
+++ demo/multi-chart-demo.js
@@ -4,13 +4,12 @@ import '@vaadin/vaadin-tabs';
 import 'api-viewer-element';
 import { Router } from '@vaadin/router';
 
-import { installMediaQueryWatcher } from 'pwa-helpers/media-query.js';
 
 
 import './helper-demo.js';
 import './chart-demo.js';
 
-import {github, preignition} from '@preignition/preignition-demo';
+import {github, preignition, DemoRoot} from '@preignition/preignition-demo';
 
 // import './src/demo-api-viewer.js';
 // import './src/demo-container.js';
@@ -21,81 +20,21 @@ import {github, preignition} from '@preignition/preignition-demo';
  * This component combines all the examples to be displayed. See the basic/intermediate/advanced folders for the actual examples.
  */
 
-class OpenWcDemo extends LitElement {
-  static get styles() {
-    return [
-      css `
-        :host {
-          display: block;
-          margin: 20px ;
-          color: var(--primary-text-color);
-        }
+class DemoChart extends DemoRoot {
 
-        h2 {
-          font-size: 20px;
-          color: var(--primary-color);
-        }
 
-        h1 {
-          align-self: center;
-          margin: 0;
-          color: var(--primary-color);
-          font-size: 60px;
-          font-weight: 400;
-          letter-spacing: -.018em;
-          line-height: 55px;
-        }
-
-        #header {
-          display: flex;
-        }
-
-        a {
-          text-decoration: none;
-        }
-
-        a:visited {
-          color: #217FF9;
-        }
-
-        #header h1 { 
-          flex: 1; 
-          padding-left: 50px;
-        }
-
-        .github {
-          transform: scale(1.2, 1.2);
-          align-self: center;
-        }
-        
-        .nav { margin-bottom: 20px; }
-        .footer { text-align: center; color: #a8a8a8;}
-      `,
-    ];
-  }
-
-  static get properties() {
-    return {
-      activeTab: { type: String },
-      tabs: { type: Array },
-      smallScreen: { type: Boolean }
-    };
-  }
 
   constructor() {
     super();
-    this.activeTab = location.pathname === '/' ? 'helper' : location.pathname.replace('/', '');
+    this.activeTab = location.pathname === '/' ? 'intro' : location.pathname.replace('/', '');
     this.tabs = ['intro', 'charts', 'container', 'helper', 'advanced'];
 
-    installMediaQueryWatcher(`(min-width: 600px)`, (matches) => {
-      this.smallScreen = !matches;
-    });
   }
 
   firstUpdated() {
     const router = new Router(this.shadowRoot.getElementById('outlet'));
     router.setRoutes([
-      { path: '/', component: 'helper-demos' },
+      { path: '/', component: 'demo-readme' },
       { path: '/intro', component: 'demo-readme' },
       { path: '/chart', component: 'chart-demo' },
       { path: '/helper', component: 'helper-demo' },
@@ -105,16 +44,13 @@ class OpenWcDemo extends LitElement {
         path: '(.*)',
         redirect: '/',
         action: () => {
-          this.activeTab = 'helper';
+          this.activeTab = 'intro';
         }
       }
     ]);
   }
 
-  switchRoute(route) {
-    this.activeTab = route;
-    Router.go(`/${route}`);
-  }
+
 
   render() {
     return html `
@@ -138,9 +74,6 @@ class OpenWcDemo extends LitElement {
     `;
   }
 
-  capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
 }
 
-customElements.define('multi-chart-demo', OpenWcDemo);
+customElements.define('multi-chart-demo', DemoChart);
